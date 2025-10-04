14864e21d406d7fe06288127581864986bcf4725
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2025-10-04 21:37:17 +0700

    Update blocks.py

--- model/blocks.py
+++ model/blocks.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from backbone.custom_block import AdaDConv, LLPFConv 
+from model.custom_block import AdaDConv, LLPFConv 
 
 
 class DoubleConv(nn.Module):
