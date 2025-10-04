1f5dd8f691d267be382789d422505e861acbfaca
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2025-10-04 09:45:48 +0700

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,9 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .backbone import *  # noqa: F401,F403
-from .decoder_head import *  # noqa: F401,F403
-from .losses import *  # noqa: F401,F403
-from .neck import *  # noqa: F401,F403
-from .segmentor import *  # noqa: F401,F403
+
 __all__ = [
     'BACKBONES'
 ]
