b714b652baade44a45987d8cd9ab2a9ba2d0da62
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2025-10-04 09:14:57 +0700

    Delete dataset directory

--- dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
-from .custom import CustomDataset
-
-__all__ = [
-    'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
-    'DATASETS', 'build_dataset', 'PIPELINES'
-]
