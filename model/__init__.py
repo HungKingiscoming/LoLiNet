161875f79c86ff0e48db0bfae12a255bd4dee9bc
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2025-10-04 09:43:53 +0700

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,12 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .backbone import *  # noqa: F401,F403
-from .builder import (BACKBONES, HEADS, LOSSES, SEGMENTORS, build_backbone,
-                      build_head, build_loss, build_segmentor)
 from .decoder_head import *  # noqa: F401,F403
 from .losses import *  # noqa: F401,F403
 from .neck import *  # noqa: F401,F403
 from .segmentor import *  # noqa: F401,F403
 __all__ = [
-    'BACKBONES', 'HEADS', 'LOSSES', 'SEGMENTORS', 'build_backbone',
-    'build_head', 'build_loss', 'build_segmentor'
+    'BACKBONES'
 ]
