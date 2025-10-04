3a61fbe0e627e8c0c164a1b81504e22a4b038a5d
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2025-10-04 16:17:50 +0700

    Update unet.py

--- model/backbone/unet.py
+++ model/backbone/unet.py
@@ -1,237 +1,48 @@
-import warnings
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.checkpoint as cp
-import numpy as np
-from mmcv.runner import BaseModule
-from mmcv.utils.parrots_wrapper import _BatchNorm
-from model.backbone.blocks import AdaDConv, SCBConvBlock
+""" Full assembly of the parts to form the complete network """
 
-from ops import Upsample  # Lớp upsampling từ OpenMMLab
+from .unet_parts import *
 
 
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
 
-class ConvBlock(nn.Module):
-    """Khối Convolution cơ bản 2 lớp, tương đương với ConvModule trong mmcv."""
-    def __init__(self, in_channels, out_channels, num_convs=2, norm_cfg=True, act_cfg=True):
-        super().__init__()
-        layers = []
-        for i in range(num_convs):
-            layers.append(
-                nn.Sequential(
-                    # Conv 3x3
-                    nn.Conv2d(in_channels if i == 0 else out_channels, out_channels, 
-                              kernel_size=3, padding=1, bias=False),
-                    # Batch Norm
-                    nn.BatchNorm2d(out_channels) if norm_cfg else nn.Identity(),
-                    # ReLU
-                    nn.ReLU(inplace=True) if act_cfg else nn.Identity(),
-                )
-            )
-        self.convs = nn.Sequential(*layers)
-        
-    def forward(self, x):
-        return self.convs(x)
-
-class UpConvBlock(nn.Module):
-    def __init__(self, in_channels, skip_channels, out_channels, 
-                 conv_block=ConvBlock, num_convs=2, stride=1, dilation=1,
-                 with_cp=False, conv_cfg=None, norm_cfg=None, act_cfg=None):
-        super().__init__()
-
-        # 1. Upsample module
-        self.upsample = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True), 
-            nn.Conv2d(in_channels, skip_channels, kernel_size=1, bias=False)
-        )
-
-        # 2. ConvBlock
-        self.conv_block = conv_block(
-            in_channels=2*skip_channels,
-            out_channels=out_channels,
-            num_convs=num_convs,
-            stride=stride,
-            dilation=dilation,
-            with_cp=with_cp,
-            conv_cfg=conv_cfg,
-            norm_cfg=norm_cfg,
-            act_cfg=act_cfg
-        )
-
-    def forward(self, skip, x):
-        x = self.upsample(x)
-        if skip.shape[-2:] != x.shape[-2:]:
-            x = F.interpolate(x, size=skip.shape[-2:], mode='bilinear', align_corners=True)
-        out = torch.cat([skip, x], dim=1)
-        out = self.conv_block(out)
-        return out
-
-class LowLightUNet(BaseModule):
-    """
-    UNet tích hợp SCB, AWD, và trả về cả enc_outs để hỗ trợ DSL.
-    """
-
-    def __init__(self,
-                 in_channels=3,
-                 base_channels=32,
-                 num_stages=5,
-                 strides=(1, 1, 1, 1, 1),
-                 enc_num_convs=(2, 2, 2, 2, 2),
-                 dec_num_convs=(2, 2, 2, 2),
-                 downsamples=(True, True, True, True),
-                 enc_dilations=(1, 1, 1, 1, 1),
-                 dec_dilations=(1, 1, 1, 1),
-                 with_cp=False,
-                 conv_cfg=None,
-                 norm_cfg=dict(type='BN'),
-                 act_cfg=dict(type='ReLU'),
-                 upsample_cfg=dict(type='InterpConv'),
-                 norm_eval=False,
-                 dcn=None,
-                 plugins=None,
-                 pretrained=None,
-                 init_cfg=None):
-        
-        super(LowLightUNet, self).__init__(init_cfg)
-
-        # --- Kiểm tra và Khởi tạo thuộc tính ---
-        self.pretrained = pretrained
-        # ... (Bỏ qua phần assert checks để giữ mã ngắn gọn, giả định input hợp lệ) ...
-
-        self.num_stages = num_stages
-        self.strides = strides
-        self.downsamples = downsamples
-        self.norm_eval = norm_eval
-        self.base_channels = base_channels
-        self.encoder = nn.ModuleList()
-        self.decoder = nn.ModuleList()
-
-        for i in range(num_stages):
-            enc_conv_block = []
-            
-            # --- Encoder Downsampling ---
-            if i != 0:
-                # THAY THẾ MaxPool2d bằng AdaDConv (AWD)
-                if strides[i] == 1 and downsamples[i - 1]:
-                    awd_in_channels = base_channels * 2**(i - 1)
-                    enc_conv_block.append(
-                        AdaDConv(
-                            in_channels=awd_in_channels,
-                            kernel_size=3,
-                            stride=2,
-                        )
-                    )
-
-                # Khối Decoder
-                upsample = (strides[i] != 1 or downsamples[i - 1])
-                self.decoder.append(
-                    UpConvBlock(
-                        conv_block=SCBConvBlock,
-                        in_channels=base_channels*2**i,
-                        skip_channels=base_channels*2**(i-1),
-                        out_channels=base_channels*2**(i-1),
-                        num_convs=dec_num_convs[i-1],
-                        stride=1,
-                        dilation=dec_dilations[i-1],
-                        with_cp=with_cp,
-                        conv_cfg=conv_cfg,
-                        norm_cfg=norm_cfg,
-                        act_cfg=act_cfg
-                    )
-                )
-
-
-            # Khối Encoder (sau Downsampling, nếu có)
-            # Tính toán in_channels cho SCBConvBlock
-            if i == 0:
-                 current_in_channels = in_channels
-            else:
-                 current_in_channels = base_channels * 2**(i - 1)
-            
-            enc_conv_block.append(
-                SCBConvBlock( # SỬ DỤNG SCBConvBlock
-                    in_channels=current_in_channels,
-                    out_channels=base_channels * 2**i,
-                    num_convs=enc_num_convs[i],
-                    # Stride chỉ áp dụng ở block đầu tiên nếu không dùng AdaDConv
-                    stride=strides[i] if (i==0 or not downsamples[i-1]) else 1,
-                    dilation=enc_dilations[i],
-                    with_cp=with_cp,
-                    conv_cfg=conv_cfg,
-                    norm_cfg=norm_cfg,
-                    act_cfg=act_cfg,
-                    dcn=None,
-                    plugins=None))
-            self.encoder.append((nn.Sequential(*enc_conv_block)))
-            in_channels = base_channels * 2**i
+        self.inc = (DoubleConv(n_channels, 64))
+        self.down1 = (Down(64, 128))
+        self.down2 = (Down(128, 256))
+        self.down3 = (Down(256, 512))
+        factor = 2 if bilinear else 1
+        self.down4 = (Down(512, 1024 // factor))
+        self.up1 = (Up(1024, 512 // factor, bilinear))
+        self.up2 = (Up(512, 256 // factor, bilinear))
+        self.up3 = (Up(256, 128 // factor, bilinear))
+        self.up4 = (Up(128, 64, bilinear))
+        self.outc = (OutConv(64, n_classes))
 
     def forward(self, x):
-        """
-        Forward function. Chỉ trả về đầu ra từ các tầng Decoder (cho Task Head).
-        """
-        # self._check_input_divisible(x) # Có thể bỏ qua nếu dùng PyTorch thuần
-        
-        enc_outs = []
-        # Encoder
-        for enc in self.encoder:
-            x = enc(x)
-            enc_outs.append(x)
-        
-        # Decoder (Bao gồm bottleneck x)
-        dec_outs = [x]
-        for i in reversed(range(len(self.decoder))):
-            x = self.decoder[i](enc_outs[i], x)
-            dec_outs.append(x)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
 
-        # Trả về TẤT CẢ các đầu ra Decoder (tương thích với nhiều Task Head)
-        return dec_outs  
-
-    def train(self, mode=True):
-        """Chuyển đổi mô hình sang chế độ training trong khi vẫn giữ freeze lớp chuẩn hóa."""
-        super(LowLightUNet, self).train(mode)
-        if mode and self.norm_eval:
-            for m in self.modules():
-                if isinstance(m, _BatchNorm):
-                    m.eval()
-
-    def _check_input_divisible(self, x):
-        h, w = x.shape[-2:]
-        whole_downsample_rate = 1
-        for i in range(1, self.num_stages):
-            # Tính toán tổng downsample rate
-            if self.strides[i] == 2 or self.downsamples[i - 1]:
-                whole_downsample_rate *= 2
-        
-        if not ((h % whole_downsample_rate == 0) and (w % whole_downsample_rate == 0)):
-            raise ValueError(
-                f'The input image size {(h, w)} should be divisible by the whole '
-                f'downsample rate {whole_downsample_rate}, when num_stages is '
-                f'{self.num_stages}, strides is {self.strides}, and downsamples '
-                f'is {self.downsamples}.'
-            )
-class TaskOnlyLoss(nn.Module):
-    def __init__(self, num_classes):
-        super(TaskOnlyLoss, self).__init__()
-        # Cross-Entropy Loss (Input: Logits (B, C, H, W), Target: Mask (B, H, W))
-        self.ce_loss = nn.CrossEntropyLoss()
-
-    def forward(self, prediction_logits, target_mask):
-        target_mask = target_mask.long() 
-        L_Task = self.ce_loss(prediction_logits, target_mask)
-        return L_Task
-    
-class SimpleTaskHead(nn.Module):
-    """
-    Chuyển đầu ra Decoder (dec_outs) thành Logits dự đoán cho Segmentation.
-    """
-    def __init__(self, in_channels, num_classes):
-        super().__init__()
-        # Dec_outs[-1] là đầu ra full size, có BASE_CHANNELS
-        self.final_conv = nn.Conv2d(in_channels, num_classes, kernel_size=1)
-
-    def forward(self, dec_outs):
-        # Đầu ra cuối cùng của Decoder (có kích thước gốc)
-        final_features = dec_outs[-1] 
-        # Logits (B, NUM_CLASSES, H, W)
-        return self.final_conv(final_features)
+    def use_checkpointing(self):
+        self.inc = torch.utils.checkpoint(self.inc)
+        self.down1 = torch.utils.checkpoint(self.down1)
+        self.down2 = torch.utils.checkpoint(self.down2)
+        self.down3 = torch.utils.checkpoint(self.down3)
+        self.down4 = torch.utils.checkpoint(self.down4)
+        self.up1 = torch.utils.checkpoint(self.up1)
+        self.up2 = torch.utils.checkpoint(self.up2)
+        self.up3 = torch.utils.checkpoint(self.up3)
+        self.up4 = torch.utils.checkpoint(self.up4)
+        self.outc = torch.utils.checkpoint(self.outc)
